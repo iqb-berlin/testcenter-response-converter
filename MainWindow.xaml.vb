8052350fd57b600e49588d0b90d79f2d24fedc9c
mechtelm <nicht@mehr.fragen>
2019-04-15 08:56:37 +0200

    Projektdateien hinzufügen.

---
+++ MainWindow.xaml.vb
@@ -0,0 +1,9 @@
+﻿Class MainWindow
+    Private Sub BtnChangeCsvSource_Click(sender As Object, e As RoutedEventArgs)
+        Me.TBCsvSource.Text = "c:\osidhfo si osidhf\isdoifs i\dsefwefe.csv"
+    End Sub
+
+    Private Sub BtnChangeBookletTxtSource_Click(sender As Object, e As RoutedEventArgs)
+        Me.TBBookletTxtSource.Text = "c:\osidhfo si osidhf\isdoifs i\dsefwefe.csv"
+    End Sub
+End Class
